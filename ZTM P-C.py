c4996e1ab00c79bdd3939d605e4c3464cb1a0e78
Sudarshen <sudarshenk@gmail.com>
2025-09-07 17:38:27 +0200

    well hello there , welcome back.

--- ZTM P-C.py
+++ ZTM P-C.py
@@ -618,3 +618,78 @@
 # ---------------------------------------------------------------
 # Lesson:  List Methods - Part 2. 
 
+# basket = ['a','b','c','d','e']
+
+# print(basket.index('d', 0, 4))
+
+
+
+# print('d' in basket)
+
+
+# print('i' in 'Hi my name is Ian.')
+
+# print(basket.count('d'))
+
+# ---------------------------------------------------------------
+# Lesson:  List Methods - Part 3.
+
+# basket = ['x','a','b','c','d','e','d']
+
+# print(basket.sort())
+
+# basket.sort()
+# print(basket)
+
+# print(sorted(basket))
+
+# sorted_list = sorted(basket)
+
+# print(sorted_list)
+
+# new_basket = basket.copy()
+# new_basket.sort()
+
+# basket.sort()
+# basket.reverse()
+
+# print(basket)
+
+# ---------------------------------------------------------------
+#
+
+# Exercise List Methods
+# SCROLL FOR ANSWERS!
+# using this list,
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+
+# 1. Remove the Banana from the list
+
+basket[0] = ""
+# print(basket)
+
+# 2. Remove "Blueberries" from the list.
+
+basket.remove("Blueberries")
+# print(basket)
+
+# 3. Put "Kiwi" at the end of the list.
+
+basket.append("Kiwi")
+# print(basket)
+
+# 4. Add "Apples" at the beginning of the list
+
+basket.insert(0, "Apples")
+# print(basket)
+
+# 5. Count how many apples in the basket
+
+app_count = basket.count("Apples")
+# print(app_count)
+
+# 6. empty the basket
+
+basket[:] = []
+print(basket)
+
---
+++ refresshing stuff.py
@@ -0,0 +1,52 @@
+# Fundamental Data Types:
+
+# - int, -float, -bool, -str, -list, -tuple, -set, -dict.
+
+# Classes --> custom types
+
+# Specialized Data Types - they are not build in python but are special mudules that you can add, its like extension
+
+# None = Means nothing, is like the idea of zero. its the absents of value.
+
+# Numbers
+
+
+# Math functions
+
+# Operator precedence
+
+# Exercise
+
+# optional: bin() and complex
+
+# Variables
+
+# Expressions vs Statement
+
+# AuGmented Assignment Operator
+
+# Strings
+
+# String concatenation
+
+# type conversion
+
+# escape sequences
+
+# formatted strings
+
+# string indexes
+
+# immutability
+
+# built-In Functions + Methods
+
+# Booleans
+
+# Exercise : type conversions
+
+#
+
+
+
+
