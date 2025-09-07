01b4393be70e08e9b61ef8f0d006a2aea53e977c
Sudarshen <sudarshenk@gmail.com>
2025-09-07 17:41:04 +0200

    wel third time should hopefully work.

--- ZTM P-C.py
+++ ZTM P-C.py
@@ -658,38 +658,43 @@
 # ---------------------------------------------------------------
 #
 
-# Exercise List Methods
-# SCROLL FOR ANSWERS!
-# using this list,
-basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+# # Exercise List Methods
+# # SCROLL FOR ANSWERS!
+# # using this list,
+# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
 
-# 1. Remove the Banana from the list
+# # 1. Remove the Banana from the list
 
-basket[0] = ""
+# basket[0] = ""
+# # print(basket)
+
+# # 2. Remove "Blueberries" from the list.
+
+# basket.remove("Blueberries")
+# # print(basket)
+
+# # 3. Put "Kiwi" at the end of the list.
+
+# basket.append("Kiwi")
+# # print(basket)
+
+# # 4. Add "Apples" at the beginning of the list
+
+# basket.insert(0, "Apples")
+# # print(basket)
+
+# # 5. Count how many apples in the basket
+
+# app_count = basket.count("Apples")
+# # print(app_count)
+
+# # 6. empty the basket
+
+# basket[:] = []
 # print(basket)
 
-# 2. Remove "Blueberries" from the list.
 
-basket.remove("Blueberries")
-# print(basket)
+# ---------------------------------------------------------------
+# Lesson:
 
-# 3. Put "Kiwi" at the end of the list.
-
-basket.append("Kiwi")
-# print(basket)
-
-# 4. Add "Apples" at the beginning of the list
-
-basket.insert(0, "Apples")
-# print(basket)
-
-# 5. Count how many apples in the basket
-
-app_count = basket.count("Apples")
-# print(app_count)
-
-# 6. empty the basket
-
-basket[:] = []
-print(basket)
 
